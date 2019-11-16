30472bd0dde3e1b0457d139ce9e319efc296aaaa
hsuan <lihsuan0723@gmail.com>
2019-11-16 13:43:18 +0800

    init

---
+++ vue.config.js
@@ -0,0 +1,15 @@
+// module.exports = {
+//   // publicPath: "./",
+//   pages: {
+//     view: {entry: "src/main.js"},
+//   },
+//   //生产环境不生成.map文件
+//   productionSourceMap: false,
+//   configureWebpack: {
+//     optimization: {
+//       //禁用SplitChunks插件，以避免生成桌面端、移动端公共的vendor文件
+//       //对单页面应用来说，其实是没必要的
+//       splitChunks: false,
+//     }
+//   }
+// };
